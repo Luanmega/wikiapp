bac7600db71233210f9d676156f5a044e07f18ae
Luis Medina <luanmega@192.168.1.11>
2024-03-03 10:02:35 -0700

    urls

---
+++ faq/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("<int:month>", views.faq_monthly_by_number),
+    path("<str:month>", views.monthly_faqs)
+]
--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_faqs_dic = {
+    "january": "january works",
+    "february": "February works!!",
+    "october": "October ok"
+}
 
 # Create your views here.
+
+
+def faq_monthly_by_number(request, month):
+    return HttpResponse(month)
+
+#regresa texto por mes elegido en la url del sitio
+def monthly_faqs(request, month):
+    try:
+        faq_text = monthly_faqs_dic[month]
+        return HttpResponse(faq_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
