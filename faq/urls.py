9e6308b2070bdf45bae10b5c322783ae69c0df31
Luis Medina <luanmega@192.168.1.12>
2024-03-11 20:07:07 -0700

    chapter 2

--- faq/urls.py
+++ faq/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("<int:month>", views.faq_monthly_by_number),
-    path("<str:month>", views.monthly_faqs)
+    path("<str:month>", views.monthly_faqs, name="month-faq")
 ]
--- faq/views.py
+++ faq/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_faqs_dic = {
     "january": "january works",
@@ -9,14 +10,22 @@ monthly_faqs_dic = {
 
 # Create your views here.
 
-
 def faq_monthly_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_faqs_dic.keys())
+
+    if month > len(monthly_faqs_dic):
+        return HttpResponseNotFound("Month not valid")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-faq", args=[redirect_month]) #how to build a full path -> /faq/january
+
+    return HttpResponseRedirect(redirect_path)
 
 #regresa texto por mes elegido en la url del sitio
 def monthly_faqs(request, month):
     try:
         faq_text = monthly_faqs_dic[month]
-        return HttpResponse(faq_text)
+        response_data = f"<h1>{faq_text}</h1>" #return html instead of plain text
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
